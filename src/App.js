e0a00013afd1263befbef7c31c8dd4ced81f54b0
yaseen shaik <yaseenshaik602@gmail.com>
2024-05-05 07:50:12 +0000

    solutions

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+import TravelGuide from './components/TravelGuide'
+// Replace your code here
+const App = () => (
+  <div>
+    <TravelGuide />
+  </div>
+)
+export default App
